4ef28314fd1b462886344c95e21d6e81332307e2
Kumar Swamy R Y <kumarswamyry82@gmail.com>
2025-11-30 11:36:43 +0530

    added DOM element references for buttons and password input

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
-
+const generateButton = document.getElementById("generateButton");
+const copyButton = document.getElementById("copyButton");
+const show = document.querySelector(".show");
+const passwordInput = document.getElementById("password");
